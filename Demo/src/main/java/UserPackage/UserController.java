58224eaa7c2ff49353a4d68198e48e59d0f20a1f
Nirmal Chathuranga <149300352+Chathuranga23603172@users.noreply.github.com>
2025-05-13 21:08:46 +0530

    Chathuranga

--- Demo/src/main/java/UserPackage/UserController.java
+++ Demo/src/main/java/UserPackage/UserController.java
@@ -6,8 +6,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import staffPackage.DBConnection;
-import staffPackage.StaffModel;
+
 
 public class UserController {
 	
